ba767e88b5af0ec33c7ae62874e83c7ec0fd9495
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-09-24 21:50:47 +0400

    Add Tapir dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,29 +11,34 @@ object Dependencies {
     val PostgresqlDriver = "42.6.0"
     val Doobie = "1.0.0-RC4"
     val Hikari = "5.0.1"
+    val Tapir = "1.7.4"
   }
 
   val catsEffect = Seq(
     // "core" module - IO, IOApp, schedulers
     // This pulls in the kernel and std modules automatically.
-    "org.typelevel" %% "cats-effect",
+    "cats-effect",
     // concurrency abstractions and primitives (Concurrent, Sync, Async etc.)
-    "org.typelevel" %% "cats-effect-kernel",
+    "cats-effect-kernel",
     // standard "effect" library (Queues, Console, Random etc.)
-    "org.typelevel" %% "cats-effect-std"
-  ).map(_ % Version.CatsEffect)
+    "cats-effect-std"
+  ).map("org.typelevel" %% _ % Version.CatsEffect)
   val catsCore = "org.typelevel" %% "cats-core" % Version.CatsCore
   val munitCatsEffect = "org.typelevel" %% "munit-cats-effect-3" % Version.MunitCatsEffect
   val flywayDb = "org.flywaydb" % "flyway-core" % Version.FlywayDb
   val pureconfigCore = "com.github.pureconfig" %% "pureconfig-core" % Version.PureconfigCore
   val postgresqlDriver = "org.postgresql" % "postgresql" % Version.PostgresqlDriver
   val doobie = Seq(
-    "org.tpolecat" %% "doobie-core",
-    "org.tpolecat" %% "doobie-hikari",
-    "org.tpolecat" %% "doobie-postgres"
-  ).map(_ % Version.Doobie)
+    "doobie-core",
+    "doobie-hikari",
+    "doobie-postgres"
+  ).map("org.tpolecat" %% _ % Version.Doobie)
    // Postgres driver 42.6.0 + type mappings.
   val hikari = "com.zaxxer" % "HikariCP" % Version.Hikari
+  val tapir = Seq(
+    "tapir-core",
+    "tapir-http4s-server"
+  ).map("com.softwaremill.sttp.tapir" %% _ % Version.Tapir)
 
   val dependencies = catsEffect ++ Seq(catsCore) ++ Seq(flywayDb) ++ Seq(pureconfigCore) ++ Seq(postgresqlDriver) ++ doobie ++ Seq(hikari)
 
